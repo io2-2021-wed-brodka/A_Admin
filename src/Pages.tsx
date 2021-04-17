307a5072529006751bd30d76494f613fac3bd170
Jan Kupis <janekkupis@gmail.com>
2021-04-17 21:28:16 +0200

    Implement sample navigation scheme

--- src/Pages.tsx
+++ src/Pages.tsx
@@ -1,19 +1,40 @@
-import React from "react";
-import {
-    BrowserRouter,
-    Route,
-    Switch
-} from "react-router-dom";
-import MainPage from './pages/MainPage';
+import { createStyles, makeStyles, Theme, Toolbar } from "@material-ui/core";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import NavigationDrawer from "./layout/NavigationDrawer";
+import Topbar from "./layout/Topbar";
+import BikePage from "./pages/BikePage";
+import StationPage from "./pages/StationPage";
+import TechPage from "./pages/TechPage";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+  })
+);
 
 const Pages = () => {
-    return(
-<BrowserRouter>
-    <Switch>
-    <Route exact path="/" component={() => <MainPage/>}/>
-    </Switch>
-        </BrowserRouter>
-    )
-}
+  const classes = useStyles();
+  return (
+    <BrowserRouter>
+      <div className={classes.root}>
+        <Topbar />
+        <NavigationDrawer />
+        <div>
+          <Toolbar />
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/bikes" />
+            </Route>
+            <Route path="/bikes" component={() => <BikePage />} />
+            <Route path="/techs" component={() => <TechPage />} />
+            <Route path="/stations" component={() => <StationPage />} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default Pages;
