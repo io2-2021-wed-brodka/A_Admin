44f94ac21a39d4e9177061175df08b211a244a80
Jan Kupis <janekkupis@gmail.com>
2021-04-20 21:18:54 +0200

    Add the bike list

--- src/pages/BikePage.tsx
+++ src/pages/BikePage.tsx
@@ -1,17 +1,101 @@
+import { Button, Grid } from "@material-ui/core";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core";
+import { useSnackbar } from "notistack";
+import React, { useEffect, useState } from "react";
+import { getAllBikes } from "../api/bikes/getBikes";
+import { Bike } from "../models/bike";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     content: {
       flexGrow: 1,
-      padding: theme.spacing(3),
+      padding: theme.spacing(3),      
+      display: "flex",
+      width: "100%",
+    },
+    table: {
+      minWidth: 650,
+    },
+    addButton: {
+      margin: theme.spacing(2),
     },
   })
 );
 
 const BikePage = () => {
   const classes = useStyles();
-  return <div className={classes.content}>Bikes</div>;
+  const { enqueueSnackbar } = useSnackbar();
+  const [rentedBikes, setRentedBikes] = useState<Bike[]>([]);
+
+  useEffect(() => {
+    getAllBikes().then((res) => {
+      if (res.isError) {
+        enqueueSnackbar("Could not get all bikes", { variant: "error" });
+      } else {
+        setRentedBikes(res.data || []);
+      }
+    });
+  }, [enqueueSnackbar]);
+
+  const deleteBike = (id: string) => {};
+
+  return (
+    <Grid container justify="center" alignItems="center" className={classes.content}>
+      <div>
+        <Button
+          className={classes.addButton}
+          variant="contained"
+          color="primary"
+        >
+          Add
+        </Button>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Bike</TableCell>
+                <TableCell align="right">Station</TableCell>
+                <TableCell align="right">User</TableCell>
+                <TableCell align="right">Status</TableCell>
+                <TableCell align="center">Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rentedBikes.map((bike) => (
+                <TableRow key={bike.id}>
+                  <TableCell component="th" scope="row">
+                    Bike {bike.id}
+                  </TableCell>
+                  <TableCell align="right">
+                    {bike.station?.name ?? "-"}
+                  </TableCell>
+                  <TableCell align="right">{bike.user?.name ?? "-"}</TableCell>
+                  <TableCell align="right">{bike.status}</TableCell>
+                  <TableCell align="center">
+                    <Button
+                      onClick={deleteBike.bind(this, bike.id)}
+                      color="secondary"
+                    >
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </Grid>
+  );
 };
 
 export default BikePage;
