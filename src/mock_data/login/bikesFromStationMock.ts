aa0f4bdd6aae0063f6921cb7e494037a3e158855
Jan Kupis <janekkupis@gmail.com>
2021-04-20 21:18:17 +0200

    Add mock data

---
+++ src/mock_data/login/bikesFromStationMock.ts
@@ -0,0 +1,10 @@
+import { IApiResponse } from "../../api/apiUtils";
+import { Token } from "../../api/login/login";
+
+export const loginMock = (username: string, password: string): IApiResponse<Token> => {
+    return {
+        isError: false,
+        responseCode: 200,
+        data: { token: "token" }
+    };
+}
