0a15dfffb850ccf41ec48c082ca3e459d824daa9
Jan Kupis <janekkupis@gmail.com>
2021-04-20 21:17:57 +0200

    Create methods that call the API

---
+++ src/api/bikes/getBikes.ts
@@ -0,0 +1,24 @@
+import { allBikesMock } from "../../mock_data/bikes/allBikes";
+import { Bike } from "../../models/bike"
+import { bikes } from "../apiUrls";
+import { handleError, handleResponse, IApiResponse } from "../apiUtils";
+import { getToken } from "../login/token";
+
+export const getAllBikes = async (): Promise<IApiResponse<Bike[]>> => {
+
+    if (parseInt(process.env.REACT_APP_MOCK_DATA || "0") === 1 || process.env.REACT_APP_BACKEND_URL === undefined)
+    {
+        return allBikesMock();
+    }        
+
+    let url = process.env.REACT_APP_BACKEND_URL + bikes;
+    type T = IApiResponse<Bike[]>;
+    return fetch(url, {
+        method: "GET",        
+        headers: new Headers({
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': getToken(),
+        }),
+    }).then<T>(handleResponse).catch<T>(handleError);
+}
