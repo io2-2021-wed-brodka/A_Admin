ce6341f66afdf26d0cebcd953f0de617a9b45e24
Jan Kupis <janekkupis@gmail.com>
2021-04-20 21:30:27 +0200

    Add tests

---
+++ src/__tests__/test-utils.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "@material-ui/core";
+import { render, RenderOptions } from "@testing-library/react";
+import { SnackbarProvider } from "notistack";
+import React, { ReactElement } from "react";
+
+const AllTheProviders = ({ children }: any) => {
+  return <SnackbarProvider maxSnack={3}>{children}</SnackbarProvider>;
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "queries">
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from "@testing-library/react";
+export { customRender as render };
