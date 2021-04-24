89ebb61eca1094c0604babfa71e824d5e9597343
Jan Kupis <janekkupis@gmail.com>
2021-04-24 11:39:43 +0200

    Fix mocks consistency

---
+++ src/mock_data/bikes/addBike.ts
@@ -0,0 +1,11 @@
+import { IApiResponse } from "../../api/apiUtils";
+import { Bike } from "../../models/bike";
+
+export const addBikeMock = (): IApiResponse<Bike> =>
+{
+    return { 
+        isError: false,            
+        responseCode:201,
+        data: { id: "1", status: "rented", user: undefined, station: { id: "1", name: "Rondo ONZ", status: "active", activeBikesCount: 5 } },
+    };
+}
